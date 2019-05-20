f30f63160fbb0cb41ccb5e0477c1888362cd224b
gywlssw <tarahjjeon96@gmail.com>
2019-05-20 23:09:37 +0900

    카드추가 코스그림디자인은 좀 더 해야겠다

--- TeamProject_20191/app/src/main/java/com/example/teamproject_2019_1/MainActivity.kt
+++ TeamProject_20191/app/src/main/java/com/example/teamproject_2019_1/MainActivity.kt
@@ -22,6 +22,8 @@ class MainActivity : AppCompatActivity() {
         crsCards.layoutManager=layoutManager
         adapter= courseAdapter(data)
         crsCards.adapter=adapter
+        data.add(0,course("추천 코스 4: 광화문 코스","거리: 7km","예상 소요 시간 : 50-60 분"))
+        data.add(0,course("추천 코스 3: 상암동 코스","거리: 9.7 km","예상 소요 시간 : 50-60 분"))
         data.add(0,course("추천 코스 2: 양재 시민의 숲 코스","거리: 7.83 km","예상 소요 시간 : 30-40 분"))
         data.add(0,course("추천 코스 1 : 여의도 ~ 반포 코스","거리: 11.05 km","예상 소요 시간 : 44 분"))
 
