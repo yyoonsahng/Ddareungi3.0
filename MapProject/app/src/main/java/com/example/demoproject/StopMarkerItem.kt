d1be9e75a055b5b400f5eaf24e1a0427885e17bd
BaeHongHyeok <bhh970207@gmail.com>
2019-05-21 14:27:54 +0900

    removed MapProject directory

--- MapProject/app/src/main/java/com/example/demoproject/StopMarkerItem.kt
+++
@@ -1,6 +0,0 @@
-package com.example.demoproject
-
-import com.google.android.gms.maps.model.LatLng
-
-data class StopMarkerItem(val id: Int = 0, val name: String = "", val latLng: LatLng = LatLng(0.0, 0.0), var bikeNum: Int = 0) {
-}
