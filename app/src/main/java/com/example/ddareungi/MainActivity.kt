7c45696e88a21f57fcc1a4ca8d3b2e153aefd263
gywlssw <tarahjjeon96@gmail.com>
2019-06-05 03:06:08 +0900

    코스 자료 입력 완료

--- app/src/main/java/com/example/ddareungi/MainActivity.kt
+++ app/src/main/java/com/example/ddareungi/MainActivity.kt
@@ -16,6 +16,7 @@ import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.example.ddareungi.dataClass.*
@@ -74,7 +75,7 @@ class MainActivity : AppCompatActivity(), BookmarkFragment.BookmarkToMapListener
         checkUserState()
         initPermission()
         checkNetwork()
-        init()
+        //init()
         readFile()
     }
 
